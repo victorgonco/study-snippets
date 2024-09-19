bea8ca373e90305f97cfcdb7971d84f18cbaecb7
victorgonco <gonco.dev@outlook.com>
2024-09-19 14:44:56 -0300

    First commit

---
+++ src/main/java/com/victorgonco/study_snippets/StudySnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.victorgonco.study_snippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySnippetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorgonco/study_snippets/StudySnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorgonco.study_snippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
